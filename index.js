e94ad3fe525f7faa67a0103282fb6967f401f625
Henry Nguyen <belugas2005@gmail.com>
2023-11-22 08:47:10 -0600

    Revert changes to env var

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const Directors = Models.Director;
 
 const app = express();
 
-mongoose.connect(process.env.CONNECTION_URI || 3000, {
+/* mongoose.connect("mongodb://127.0.0.1:27017/myFlixDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); */
+
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
