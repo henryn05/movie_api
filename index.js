23164efd2adfab4242e59cd14d0e51cd92231aae
Henry Nguyen <belugas2005@gmail.com>
2023-12-05 06:23:49 -0600

    Added allowedOrigins array

--- index.js
+++ index.js
@@ -35,12 +35,14 @@ app.use(express.json());
 app.use(methodOverride("_method"));
 
 const cors = require("cors");
+
 app.use(
   cors({
     origin: (origin, callback) => {
+      const allowedOrigins = ["http:localhost1234"];
+
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
         let message =
           "The CORS policy for this application doesn't allow access from origin " +
           origin;
@@ -241,33 +243,27 @@ app.delete(
 );
 
 // Routes for Movies
-app.get(
-  "/movies",
-  async (req, res) => {
-    await Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", async (req, res) => {
+  await Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
-app.get(
-  "/movies/:Title",
-  async (req, res) => {
-    await Movies.findOne({ Title: req.params.Title })
-      .then((user) => {
-        res.json(user);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies/:Title", async (req, res) => {
+  await Movies.findOne({ Title: req.params.Title })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 app.post(
   "/movies",
