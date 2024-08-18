0458e2d765a7e32edd21cf4e20e7e037eb2655d0
Henry Nguyen <belugas2005@gmail.com>
2024-08-18 14:44:47 -0500

    Revert changes

--- index.js
+++ index.js
@@ -13,12 +13,11 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-const app = express();
-
-/* mongoose.connect("mongodb://127.0.0.1:27017/myFlixDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}); */
+// const app = express();
+//  mongoose.connect("mongodb://127.0.0.1:27017/myFlixDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
